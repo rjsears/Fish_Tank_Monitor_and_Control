f6579d15810be8203a7e3e7ca7e096b5b2513995
rjsears <richardjsears@gmail.com>
2020-06-06 10:06:48 -0700

    Delete run.py

--- tank_control/run.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-__author__ = 'Richard J. Sears'
-VERSION = "V1.0 (2020-06-06)"
-
-from tank_control import app
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
